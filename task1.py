5fc376b020a558bf0b187eebfc730889ba08e240
snob24zp <snob24zp@proton.me>
2024-07-17 22:56:52 +0300

    class for task2

--- task1.py
+++ task1.py
@@ -3,124 +3,141 @@ import os
 import shutil
 import hashlib
 
-cur_dir=os.getcwd()
 
-print(f"cur_dir {cur_dir}")
 
-parser=argparse.ArgumentParser(description="File rearange")
-parser.add_argument("source",     type=str,nargs='?', default=cur_dir, help="source dir")
-parser.add_argument("destination",type=str,nargs='?', default='..\\dist',  help="source dir")
+class FilesRearrange:
+    def __init__(self,source,destination):
+        self.dublicates={}
+        self.source=source
+        self.destination=destination
 
-args=parser.parse_args()
+    def get_hash(self,path):
+        """Повертає хеш-значення для файлу."""
+        with open(path, 'rb') as file:
+            bytes = file.read() 
+            readable_hash = hashlib.sha256(bytes).hexdigest()
+            return readable_hash
 
-print(args.source)
-print(args.destination)
+    def copydublicatefile(self,n:int,filepathname:str,source:str):
+        """file is copied
+        adding a number in brackets to the file name."""
+        basename=os.path.basename(filepathname)
+        name, ext = os.path.splitext(basename)
+        name+=f"({n})"
+        filepathnamecopy=os.path.join(os.path.dirname(filepathname),name+ext)
+        if os.path.isfile(filepathnamecopy):
+            self.copydublicatefile(n+1,filepathname,source)
+        else:
+            shutil.copy(source,filepathnamecopy)
 
-try:
-    os.makedirs(args.destination,exist_ok=True)
-except PermissionError as e:
-    print(e)
-except OSError as e:
-    print(e)
-
-def get_hash(path):
-    """Повертає хеш-значення для файлу."""
-    with open(path, 'rb') as file:
-        bytes = file.read() 
-        readable_hash = hashlib.sha256(bytes).hexdigest();
-        return readable_hash
-
-dublicates={}
-
-def copydublicatefile(n:int,filepathname:str,source:str):
-    basename=os.path.basename(filepathname)
-    name, ext = os.path.splitext(basename)
-    name+=f"({n})"
-    filepathnamecopy=os.path.join(os.path.dirname(filepathname),name+ext)
-    if os.path.isfile(filepathnamecopy):
-        copydublicatefile(n+1,filepathname,source)
-    else:
-        shutil.copy(source,filepathnamecopy)
-
-def copyfile(source):
-    try:
-        print(source)
-        _,ext=os.path.splitext(source)
-        filepath= args.destination if ext=="" else os.path.join(args.destination,ext[1:])
-        filename= os.path.basename(source)
-        os.makedirs(filepath,exist_ok=True)
-        filepathname=os.path.join(filepath,filename)
-        if os.path.isfile(filepathname):
-            if filepathname not in dublicates:
-                dublicates[filepathname]=set()
-                dublicates[filepathname].add(get_hash(filepathname))
-            new_hash=get_hash(source)
-            if new_hash in dublicates[filepathname]:
-                print(f"dublicates: {source}")
+    def CopyFile(self,source):
+        """tries to copy a file 
+        and checks whether a file with the same name exists in the destination directory.
+        Checks the new file is the same as in the destination folder or different.
+        if the file is the same, then copying does not occur.
+        if the file is different, then the file is copied
+        File comparison occurs by calculating the hash function. 
+        The Hash function is stored in a dictionary. 
+        the key in the dictionary is the name of the file in the destination directory.
+        The value in the dictionary is a set of hash functions 
+        for all files with this name that have previously been processed"""
+        try:
+            print(source)
+            _,ext=os.path.splitext(source)
+            filepath= self.destination if ext=="" \
+                                            else os.path.join(self.destination,ext[1:])
+            filename= os.path.basename(source)
+            os.makedirs(filepath,exist_ok=True)
+            filepathname=os.path.join(filepath,filename)
+            if os.path.isfile(filepathname):
+                if filepathname not in self.dublicates:
+                    self.dublicates[filepathname]=set()
+                    self.dublicates[filepathname].add(self.get_hash(filepathname))
+                new_hash=self.get_hash(source)
+                if new_hash in self.dublicates[filepathname]:
+                    print(f"dublicates: {source}")
+                else:
+                    print(f"different file with same name: {source}")
+                    self.dublicates[filepathname].add(new_hash)
+                    self.copydublicatefile(1,filepathname,source)
             else:
-                print(f"different file with same name: {source}")
-                dublicates[filepathname].add(new_hash)
-                copydublicatefile(1,filepathname,source)
-        else:
-            shutil.copy(source,filepathname)
-        
-    except Exception as e:
-        print(e)
+                shutil.copy(source,filepathname)
+            
+        except FileNotFoundError as e:
+            print(e)
+        except PermissionError as e:
+            print(e)
+        except OSError as e:
+            print(e)
+        except Exception as e:
+            print(e) 
 
 
 
+    def FileRearrange(self,source):   
+        """iterates through all files in the input directory 
+        when encountering a subdirectory calls itself recursively 
+        If a file is encountered calls the file copy function""" 
+        try:
+            dir_contents = os.listdir(source) 
+            try:
+                for item in dir_contents:
+                    filepath=os.path.join(source,item)
+                    if os.path.isdir(filepath):
+                        self.FileRearrange(filepath)
+                    elif os.path.isfile(filepath):
+                        self.CopyFile(filepath)
+                    else:
+                        print(f"Non recognition item: {filepath}")       
+            except FileNotFoundError as e:
+                    print(e)
+            except PermissionError as e:
+                print(e)
+            except OSError as e:
+                print(e)
+            except Exception as e:
+                print(e)          
 
-def file_rearrange(source):    
-    # try:
-    #     dir_contents = os.listdir(source)    
-    # # except FileNotFoundError as e:
-    # #     print(e)
-    # except PermissionError as e:
-    #     print(e)
-    # except OSError as e:
-    #     print(e)
-    # except Exception as e:
-    #     print(e)    
-
-    dir_contents = os.listdir(source) 
-
-    for item in dir_contents:
-        filepath=os.path.join(source,item)
-        if os.path.isdir(filepath):
-            file_rearrange(filepath)
-        elif os.path.isfile(filepath):
-            copyfile(filepath)
-        else:
-            print(f"Non recognition item: {filepath}")
+        except PermissionError as e:
+            print(e)
+        except OSError as e:
+            print(e)
+        except Exception as e:
+            print(e)    
 
 
-file_rearrange(args.source)
 
-# import hashlib
-# import os
+    def FilesRearrange(self):
+        """ creates an output directory and runs the main processing logic"""    
+        try:
+            os.makedirs(self.destination,exist_ok=True)
+            self.FileRearrange(self.source)
+        except PermissionError as e:
+            print(e)
+        except OSError as e:
+            print(e)
+        except Exception as e:
+            print(e)  
 
-# def get_hash(path):
-#     """Повертає хеш-значення для файлу."""
-#     with open(path, 'rb') as file:
-#         bytes = file.read() 
-#         readable_hash = hashlib.sha256(bytes).hexdigest();
-#         return readable_hash
 
-# def find_duplicates(directory):
-#     """Шукає дублікати в директорії."""
-#     hashes = {}
-#     duplicates = []
-#     for dirpath, dirnames, filenames in os.walk(directory):
-#         for filename in filenames:
-#             path = os.path.join(dirpath, filename)
-#             file_hash = get_hash(path)
-#             if file_hash not in hashes:
-#                 hashes[file_hash] = path
-#             else:
-#                 duplicates.append((path, hashes[file_hash]))
-#     return duplicates
+if __name__=="__main__":
 
-# # Пошук дублікатів у заданій директорії
-# duplicates = find_duplicates('/path/to/directory')
-# for duplicate in duplicates:
-#     print(f"Дублікатні файли: {duplicate[0]} та {duplicate[1]}")
+
+    cur_dir=os.getcwd()
+
+    print(f"cur_dir {cur_dir}")
+
+    parser=argparse.ArgumentParser(description="File rearange")
+    parser.add_argument("source",     type=str,nargs='?', default=cur_dir, help="source dir")
+    parser.add_argument("destination",type=str,nargs='?', default='..\\dist',  help="source dir")
+
+    args=parser.parse_args()
+
+    print(args.source)
+    print(args.destination)
+
+    FA=FilesRearrange(args.source,args.destination)
+
+    FA.FilesRearrange()
+
+
